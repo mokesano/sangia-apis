4c7a3b11f09001388e156b4afe18210c518cfb90
Rochmady <kampo.mokesano@gmail.com>
2026-05-11 18:16:31 +0700

    Add file sdg10

---
+++ core/Modules/SDG/Config/Dictionaries/Sdg10.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+return [
+    // Bahasa Inggris
+    "reduced inequalities", "migration", "income inequality", "social inclusion", "equality",
+    "equal opportunity", "social protection", "fiscal policy", "discriminatory policies", "social inequality",
+    "economic inequality", "wage gap", "social disparity", "economic disparity", "social exclusion",
+    "marginalized", "social mobility", "wealth distribution", "income distribution", "migrant rights",
+    "minority rights", "racial equality", "gender equality", "social equity", "economic empowerment",
+    "inclusive society", "wage discrimination", "social status", "socioeconomic status", "disadvantaged groups",
+    "affirmative action", "economic opportunity", "inequality reduction",
+    
+    // Bahasa Indonesia
+    "ketimpangan berkurang", "migrasi", "ketimpangan pendapatan", "inklusi sosial", "kesetaraan",
+    "kesetaraan kesempatan", "perlindungan sosial", "kebijakan fiskal", "kebijakan diskriminatif",
+    "kesenjangan sosial", "pengucilan sosial", "disparitas ekonomi", "kelompok terpinggirkan",
+    "distribusi pendapatan", "mobilitas sosial", "hak minoritas", "hak migran", "kesetaraan ras",
+    "kebijakan afirmatif", "pemberdayaan ekonomi", "masyarakat inklusif"
+];
