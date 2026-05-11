ea9b8c21b7598b0916eaa286ba0dde35c6302e28
Rochmady <kampo.mokesano@gmail.com>
2026-05-11 18:47:25 +0700

    Resolved VersionConfig::getAllVersions() berfungsi dengan baik dan mengembalikan 7 versi (v0, v1, v2, v3, v4, v5, v5e)

--- core/Modules/SDG/Config/VersionConfig.php
+++ core/Modules/SDG/Config/VersionConfig.php
@@ -85,4 +85,16 @@ class VersionConfig
     {
         return ['v0', 'v1', 'v2', 'v3', 'v4', 'v5', 'v5e'];
     }
+
+    /**
+     * Get all available versions with their configurations
+     */
+    public static function getAllVersions(): array
+    {
+        $versions = [];
+        foreach (self::versions() as $version) {
+            $versions[$version] = self::get($version);
+        }
+        return $versions;
+    }
 }
