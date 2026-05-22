9679986b3cbd5b97a2ac00c4f8367da01b589be5
Rochmady <srochmady@gmail.com>
2026-05-22 11:34:12 +0700

    Add composer componen after install

---
+++ library/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sangia/api-engine',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '192e9a34a6d3dd37e17e10daa709d21295fd63f0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sangia/api-engine' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '192e9a34a6d3dd37e17e10daa709d21295fd63f0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
